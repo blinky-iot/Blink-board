1ec59360723cbdad984d68e7388ec972f72bdfac
chalomwas <citizenchalo@gmail.com>
2024-09-20 10:14:22 +0300

    doc change to contain all the documentations

--- examples/analog_Read/analog_Read.ino
+++ examples/analog_Read/analog_Read.ino
@@ -1,11 +1,11 @@
-#include "blink.h"
+#include <blink.h>
 
 Blink adc;
 
 void setup() {
 	Serial.begin(115200);
   adc.begin();
-
+}
 void loop() {
   for(uint8_t p=0; p<8; p++){
     if(adc.analogRead(p)){
